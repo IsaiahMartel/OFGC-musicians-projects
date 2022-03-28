d72059db9d7267694d908f0967c6423e1467411f
IsaiahMartel <91074551+IsaiahMartel@users.noreply.github.com>
2022-03-28 10:13:50 +0200

    Commit

--- frontend/src/app/views/projects/project-tabs/calendar/calendar.page.ts
+++ frontend/src/app/views/projects/project-tabs/calendar/calendar.page.ts
@@ -33,59 +33,56 @@ export class CalendarPage implements OnInit {
   loadInfo() {
     this.storage.get("scheduleProject").then(data => {
       if (data) {
-console.log(data);
+        this.scheduleArray = JSON.parse(data);
 
-
-          this.scheduleArray = JSON.parse(data);
-   
       } else {
-     
-        
+
+
         this.updateData();
       }
     })
   }
 
   deleteSchedule(idSchedule: number) {
-      this.scheduleService.deleteSchedule(idSchedule).subscribe(() => {
-        this.loadInfo();
-      });
-    }
+    this.scheduleService.deleteSchedule(idSchedule).subscribe(() => {
+      this.loadInfo();
+    });
+  }
 
 
   doConnection() {
-      const echo = new Echo({
-        broadcaster: 'pusher',
-        key: 'local',
-        wsHost: 'localhost',
-        wsPort: 6001,
-        forceTLS: false,
-        disableStats: true
-      });
+    const echo = new Echo({
+      broadcaster: 'pusher',
+      key: 'local',
+      wsHost: 'localhost',
+      wsPort: 6001,
+      forceTLS: false,
+      disableStats: true
+    });
 
-      const channel = echo.channel('channel');
-      channel.listen('Alert', (data) => {
-        console.log(JSON.stringify(data));
-        this.notification(data);
-        this.updateData();
-      });
-    }
+    const channel = echo.channel('channel');
+    channel.listen('Alert', (data) => {
+      console.log(JSON.stringify(data));
+      this.notification(data);
+      this.updateData();
+    });
+  }
 
   updateData() {
-      this.scheduleService.getSchedulesByProjectId(this.project_id).subscribe((s: Array<Schedule>) => {
-        this.storage.set("scheduleProject", JSON.stringify(s));
-        this.scheduleArray = s;
-        
-      })
-    }
+    this.scheduleService.getSchedulesByProjectId(this.project_id).subscribe((s: Array<Schedule>) => {
+      this.storage.set("scheduleProject", JSON.stringify(s));
+      this.scheduleArray = s;
+
+    })
+  }
 
   async notification(message: string) {
-      const alert = await this.alertController.create({
-        cssClass: 'my-custom-class',
-        header: 'Se han realizado cambios',
-        message: message,
-        buttons: ['OK']
-      });
-      await alert.present();
-    }
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Se han realizado cambios',
+      message: message,
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
 }
